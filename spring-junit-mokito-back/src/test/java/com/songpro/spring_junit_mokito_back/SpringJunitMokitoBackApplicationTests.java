3224dd67071a94a07df8ca079b282cbf0bc4af0b
taegeunsong <tgsong@thingspire.com>
2024-12-26 12:34:41 +0900

    #1 Spring boot 초기 셋팅

---
+++ spring-junit-mokito-back/src/test/java/com/songpro/spring_junit_mokito_back/SpringJunitMokitoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songpro.spring_junit_mokito_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJunitMokitoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
